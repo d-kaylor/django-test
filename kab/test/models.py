8a6a81fd5e4453b30bdfc8d5c4f59b704c561f93
David Kaylor <dkaylor@redhat.com>
2023-03-24 16:32:54 -0400

    Add csrf domain and test db change

--- kab/test/models.py
+++ kab/test/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Test(models.Model):
     test1 = models.CharField(max_length=20)
+    test2 = models.CharField(max_length=20, null=True)
