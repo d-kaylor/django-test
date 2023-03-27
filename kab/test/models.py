3a78c15b4939fe6743282f0d1fed7432411b7142
David Kaylor <dkaylor@redhat.com>
2023-03-27 16:55:29 -0400

    Test db change

--- kab/test/models.py
+++ kab/test/models.py
@@ -3,4 +3,3 @@ from django.db import models
 # Create your models here.
 class Test(models.Model):
     test1 = models.CharField(max_length=20)
-    test2 = models.CharField(max_length=20, null=True)
